48012ab2efc5efa778e543d680453d9daee63b09
Maxim <ardier16@gmail.com>
2017-04-04 23:28:23 +0300

    NURE Marks v. 0.7
    Testing version

---
+++ NUREMarks/Models/StudentData.cs
@@ -0,0 +1,11 @@
+﻿namespace NUREMarks.Models
+{
+    public class StudentData
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public double Rating { get; set; }
+        public string Info { get; set; }
+
+    }
+}
